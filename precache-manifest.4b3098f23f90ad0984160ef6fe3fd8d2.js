c108fee02118849182454794168cde9bb87e5b96
t-1000-t <goognya@gmail.com>
2019-11-07 09:50:20 +0200

    Updates

--- precache-manifest.4b3098f23f90ad0984160ef6fe3fd8d2.js
+++ precache-manifest.4b3098f23f90ad0984160ef6fe3fd8d2.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7ff2b56959e668fa1624295baba1874d",
+    "revision": "46a39d35ad5c3e736ada4bd338cbd046",
     "url": "/goit-react-hw-02-reader/index.html"
   },
   {
-    "revision": "e3879ac591daf9e7e5ef",
-    "url": "/goit-react-hw-02-reader/static/css/main.3f3f5bac.chunk.css"
+    "revision": "4629653012dde94c706e",
+    "url": "/goit-react-hw-02-reader/static/css/main.72ef088a.chunk.css"
   },
   {
     "revision": "6e2d8c189ca8ccfa8fce",
     "url": "/goit-react-hw-02-reader/static/js/2.58322872.chunk.js"
   },
   {
-    "revision": "e3879ac591daf9e7e5ef",
-    "url": "/goit-react-hw-02-reader/static/js/main.551286f0.chunk.js"
+    "revision": "4629653012dde94c706e",
+    "url": "/goit-react-hw-02-reader/static/js/main.fe407da8.chunk.js"
   },
   {
     "revision": "c0f2b9a1bd18860287f3",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-02-reader/precache-manifest.5d4f52b08053d7395e2519f0b6e4c027.js"
+  "/goit-react-hw-02-reader/precache-manifest.4b3098f23f90ad0984160ef6fe3fd8d2.js"
 );
 
 self.addEventListener('message', (event) => {
